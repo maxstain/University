02d05a8b434f3d6a1bdad32de4af76cb0170800d
fchabcho <firaschabchoub@hotmail.com>
2025-12-09 14:22:23 +0100

    add spring-boot-starter-security-oauth2-resource-server dependency

--- src/main/java/org/example/university/controllers/BlocController.java
+++ src/main/java/org/example/university/controllers/BlocController.java
@@ -14,27 +14,27 @@ public class BlocController {
     public BlocController() {
     }
 
-    @PostMapping("/bloc/add")
+    @PostMapping("bloc/add")
     private void addBloc(@RequestBody Bloc bloc) {
         blocService.addBloc(bloc);
     }
 
-    @PutMapping("/bloc/update")
+    @PutMapping("bloc/update")
     private void updateBloc(@RequestBody Bloc bloc) {
         blocService.updateBloc(bloc);
     }
 
-    @DeleteMapping("/bloc/delete/{id}")
+    @DeleteMapping("bloc/delete/{id}")
     private void deleteBloc(@PathVariable long id) {
         blocService.deleteBloc(id);
     }
 
-    @GetMapping("/blocs")
+    @GetMapping("blocs")
     private List<Bloc> getAllBlocs() {
         return blocService.getAllBlocs();
     }
 
-    @GetMapping("/blocs/{id}")
+    @GetMapping("blocs/{id}")
     private Bloc getBlocById(@PathVariable long id) {
         return blocService.getBlocById(id);
     }
