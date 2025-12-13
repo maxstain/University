47671ba7688fe7cad47f8a51f69099755e3f5ba3
Firas Chabchoub <firaschabchoub@hotmail.com>
2025-12-13 18:59:06 +0100

    refactor BlocController to use IBlocService and simplify endpoint mappings

--- src/main/java/org/example/university/controllers/BlocController.java
+++ src/main/java/org/example/university/controllers/BlocController.java
@@ -1,42 +1,40 @@
 package org.example.university.controllers;
 
 import org.example.university.entities.Bloc;
-import org.example.university.services.bloc.BlocServiceImpl;
+import org.example.university.services.bloc.IBlocService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/bloc")
 public class BlocController {
 
-    BlocServiceImpl blocService;
+    IBlocService iBlocService;
 
-    public BlocController() {
+    @PostMapping("/add")
+    void addBloc(@RequestBody Bloc bloc) {
+        iBlocService.addBloc(bloc);
     }
 
-    @PostMapping("bloc/add")
-    private void addBloc(@RequestBody Bloc bloc) {
-        blocService.addBloc(bloc);
+    @PutMapping("/update")
+    void updateBloc(@RequestBody Bloc bloc) {
+        iBlocService.updateBloc(bloc);
     }
 
-    @PutMapping("bloc/update")
-    private void updateBloc(@RequestBody Bloc bloc) {
-        blocService.updateBloc(bloc);
+    @DeleteMapping("/delete/{id}")
+    void deleteBloc(@PathVariable long id) {
+        iBlocService.deleteBloc(id);
     }
 
-    @DeleteMapping("bloc/delete/{id}")
-    private void deleteBloc(@PathVariable long id) {
-        blocService.deleteBloc(id);
+    @GetMapping("/blocs")
+    List<Bloc> getAllBlocs() {
+        return iBlocService.getAllBlocs();
     }
 
-    @GetMapping("blocs")
-    private List<Bloc> getAllBlocs() {
-        return blocService.getAllBlocs();
-    }
-
-    @GetMapping("blocs/{id}")
-    private Bloc getBlocById(@PathVariable long id) {
-        return blocService.getBlocById(id);
+    @GetMapping("/{id}")
+    Bloc getBlocById(@PathVariable long id) {
+        return iBlocService.getBlocById(id);
     }
 
 }
